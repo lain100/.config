911be6d7e68777c005efdfb0cd85c891a7d14f85
lain100 <lain@Lain.localdomain>
2026-02-15 10:51:24 +0900

    at last

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -5,6 +5,7 @@ g.mapleader = " "
 g.maplocalleader = ","
 
 remap("n", "q", "<cmd>q<cr>")
+remap("n", "<c-s>", "<cmd>w<cr>")
 remap("n", "<leader>qq", "<cmd>qa!<cr>")
 remap("n", "<c-p>", "<cmd>N<cr>")
 remap("n", "<c-k>", "<cmd>n<cr>")
--- nvim/lua/plugins/autopaires.lua
+++ nvim/lua/plugins/autopaires.lua
@@ -6,6 +6,7 @@ return {
 	},
 	config = function ()
 		require("nvim-autopairs").setup({
+      ignored_next_char = "",
 			enable_check_bracket_line = false,
 		})
 		require("nvim-autopairs").get_rules("'")[1].not_filetypes = { "lisp" }
